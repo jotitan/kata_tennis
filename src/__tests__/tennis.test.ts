148913b4f0494c6cea854b7560f734c3f91647c7
jonathan.baranzini <jonathan.baranzini@talan.com>
2021-04-07 13:51:53 +0200

    Add second test

--- src/__tests__/tennis.test.ts
+++ src/__tests__/tennis.test.ts
@@ -10,4 +10,10 @@ describe('tennis', () => {
 		tennis.firstPlayerWinPoint();
 		expect(tennis.gameScore()).toEqual("fifteen - love");
 	});
+	
+	it('should allow player 2 to win a point', () => {
+		let tennis = new Tennis();
+		tennis.secondPlayerWinPoint();
+		expect(tennis.gameScore()).toEqual("love - fifteen");
+	});
 });
